bf9c65ef388eddd7b708ba19de34f59d160122f4
ritchie <ritchie-huang@outlook.com>
2016-08-02 15:59:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ritchie_huang/manyuemusic/Activity/SearchMusicActivity.java
@@ -0,0 +1,15 @@
+package com.example.ritchie_huang.manyuemusic.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ritchie-huang on 16-8-2.
+ */
+public class SearchMusicActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
